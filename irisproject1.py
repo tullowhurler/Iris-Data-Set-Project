34a17348e9798b3af573be08bd85fe58ec796321
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-19 21:36:07 +0100

    Using numpy to seperate the columns

--- irisproject1.py
+++ irisproject1.py
@@ -1,3 +1,12 @@
-#Brian Condon Iris Project
-#18/4/18 
-#f 
+ # Brian Condon
+# 19/4/18
+# Calculating mean using Numpy 
+
+# import numpy for analysis
+import numpy 
+
+# accessing data file
+data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
+
+# accessing the first column
+firstcol = data[:,0] 
