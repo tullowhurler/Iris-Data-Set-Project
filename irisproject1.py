662e32bd1280e23de844c5bea44d560794c942bf
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-19 21:56:29 +0100

    mean

--- irisproject1.py
+++ irisproject1.py
@@ -1,4 +1,4 @@
- # Brian Condon
+# Brian Condon
 # 19/4/18
 # Calculating mean using Numpy 
 
@@ -10,3 +10,8 @@ data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
 
 # accessing the first column
 firstcol = data[:,0] 
+meanfirstcol = numpy.mean(data[:,0])
+
+print("The mean sepal length of the data set is", meanfirstcol) 
+
+# Answer is 5.84
