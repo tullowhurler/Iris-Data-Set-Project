569763d1806523aec7ba7efa023b1456e1f1d07e
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-19 22:55:07 +0100

    updated with min and max and answers

--- iriscol2.py
+++ iriscol2.py
@@ -9,6 +9,11 @@ data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
 secondcol = data[:,1] 
 
 meansecondcol = numpy.mean(data[:,1])
+minsecondcol = numpy.min(data[:,1])
+
+maxsecondcol = numpy.max(data[:,1])
 
 print("The mean sepal width of the data set is",meansecondcol) #Answer is 3.054
+print("The minimum sepal width of the data set is",minsecondcol) #Answer is 2.0
+print("The maximum sepal width of the data set is",maxsecondcol) #Answer is 4.4
 
