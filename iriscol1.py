4288671a30613adc8ea88a0601acbf0ff76a40d4
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-19 22:01:24 +0100

    max

--- iriscol1.py
+++ iriscol1.py
@@ -1,3 +1,4 @@
+
 # Brian Condon
 # 19/4/18
 # Calculating mean using Numpy 
@@ -9,11 +10,15 @@ import numpy
 data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
 
 # accessing the first column
-firstcol = data[:,0] 
+firstcol = data[:,0]
+
+#Calculating mean, min and max
 meanfirstcol = numpy.mean(data[:,0])
 minfirstcol = numpy.min(data[:,0])
+maxfirstcol = numpy.max(data[:,0])
 
 print("The mean sepal length of the data set is", meanfirstcol) # Answer is 5.84
 print("The minimum sepal length of the data set is", minfirstcol) # Answer is 4.3
+print("The maximum sepal length of the data set is", maxfirstcol) #Answer is 7.9 
 
 
