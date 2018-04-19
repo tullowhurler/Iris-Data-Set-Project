c0b180601c4609f36d462497cae1626d8b062e03
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-19 21:59:13 +0100

    update and renamed

--- iriscol1.py
+++ iriscol1.py
@@ -11,7 +11,9 @@ data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
 # accessing the first column
 firstcol = data[:,0] 
 meanfirstcol = numpy.mean(data[:,0])
+minfirstcol = numpy.min(data[:,0])
 
-print("The mean sepal length of the data set is", meanfirstcol) 
+print("The mean sepal length of the data set is", meanfirstcol) # Answer is 5.84
+print("The minimum sepal length of the data set is", minfirstcol) # Answer is 4.3
 
-# Answer is 5.84
+
