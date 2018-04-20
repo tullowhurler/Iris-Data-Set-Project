f4328612c48480b24eba776b10fc7100d8696790
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-20 21:43:48 +0100

    Updated comments and references

--- iriscol3.py
+++ iriscol3.py
@@ -1,25 +1,29 @@
-#Brian Condon 
+ #Brian Condon 
 #Analysis of the third column of data
 #20/04/18
 
 # import to analyse data
 import numpy 
 
-# accessing data file
+# accessing data file https://docs.python.org/3/library/csv.html
 data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
 
+# Access the third column https://stackoverflow.com/questions/15389290/how-to-find-out-the-average-on-a-csv-file 
 thirdcol = data[:,2]
-
 meanthirdcol = numpy.mean(data[:,2])
 
 minthirdcol = numpy.min(data[:,2])
 
 maxthirdcol = numpy.max(data[:,2])
 
+#import to make the histogram
 import matplotlib.pyplot as pl 
+
+# lable the histogram and print the result 
 pl.hist(thirdcol)
 pl.show()
 
+#print the outcome
 print("The mean petal length of the data set it",meanthirdcol) #Answer 3.75cm
 print("The minimum petal length of the data set is",minthirdcol) #Answer 1.0cm
 print("The maximum petal length of the data set is",maxthirdcol) #Answer is 6.9cm
