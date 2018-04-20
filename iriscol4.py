8b22d7c67534c7eff6bb08194e897ae83e3170e7
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-20 21:25:54 +0100

    fourth column data

---
+++ iriscol4.py
@@ -0,0 +1,9 @@
+#Brian Condon 
+#Analysis of the fourth column of data
+#20/04/18
+
+# import to analyse data
+import numpy 
+
+# accessing data file
+data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
