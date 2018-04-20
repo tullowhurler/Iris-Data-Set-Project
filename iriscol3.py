055f36d77d49d0813f63e56f7b8110e5a52e064d
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-20 21:19:00 +0100

    min and max

--- iriscol3.py
+++ iriscol3.py
@@ -11,3 +11,11 @@ data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
 thirdcol = data[:,2]
 
 meanthirdcol = numpy.mean(data[:,2])
+
+minthirdcol = numpy.min(data[:,2])
+
+maxthirdcol = numpy.max(data[:,2])
+
+print("The mean petal length of the data set it",meanthirdcol) #Answer 3.75cm
+print("The minimum petal length of the data set is",minthirdcol) #Answer 1.0cm
+print("The maximum petal length of the data set is",maxthirdcol) #Answer is 6.9cm
