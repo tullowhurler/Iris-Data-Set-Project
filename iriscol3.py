dfb39e741ac3fbd45855c60a64a553410f245d55
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-20 21:24:11 +0100

    create histogram

--- iriscol3.py
+++ iriscol3.py
@@ -16,6 +16,10 @@ minthirdcol = numpy.min(data[:,2])
 
 maxthirdcol = numpy.max(data[:,2])
 
+import matplotlib.pyplot as pl 
+pl.hist(thirdcol)
+pl.show()
+
 print("The mean petal length of the data set it",meanthirdcol) #Answer 3.75cm
 print("The minimum petal length of the data set is",minthirdcol) #Answer 1.0cm
 print("The maximum petal length of the data set is",maxthirdcol) #Answer is 6.9cm
