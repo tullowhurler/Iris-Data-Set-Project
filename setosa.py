c8b585e8fefe5f633b13646aa8064999fdd1d1e8
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-23 19:28:44 +0100

    updated mean

--- setosa.py
+++ setosa.py
@@ -8,12 +8,15 @@ import numpy
 #I created another csv file after having trouble splitting up the columns to just the setosa file.
 data = numpy.genfromtxt('data/setosa.csv', delimiter = ',')
 
-# Seperate the column
+# Seperate the column and finds the mean
 setosa = data[:,0]
-
 meansetosa = numpy.mean((data[:,2]))
+
 setosapw = data[:,3]
+meansetosapw = numpy.mean(data[:,3])
 
 #excludes nan from the file so we can use numpy to develop mean etc. https://stackoverflow.com/questions/11620914/removing-nan-values-from-an-array
 setosa = setosa[~numpy.isnan(setosa)]
-print("The mean petal length of the setosa is",meansetosa)
+
+print("The mean petal length of the setosa is",meansetosa) #Answer is 1.4cm
+print("The mean petal width of the setosa is",meansetosapw) #Answer is 0.2cm
