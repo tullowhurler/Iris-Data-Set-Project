23f0236a286266e3a1e010c453038370008184f0
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-23 22:31:59 +0100

    Update setosa.py

--- setosa.py
+++ setosa.py
@@ -21,7 +21,10 @@ meansetosapw = numpy.mean(data[:,3])
 setosa = setosa[~numpy.isnan(setosa)]
 
 import matplotlib.pyplot as pl 
-pl.scatter(setosa,setosapw)
+pl.hist(setosa)
+pl.xlabel('Petal Length in cms')
+pl.ylabel('Amount of Samples')
+pl.title('The Petal Length of the Setosa Flower')
 pl.show()
 
 print("The mean petal length of the setosa is",meansetosa) #Answer is 1.4cm
