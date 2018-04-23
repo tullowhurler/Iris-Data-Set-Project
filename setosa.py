8b64f340a9e16feb7620c66ee0bb4043a2e8fe25
Brian Condon <35730844+tullowhurler@users.noreply.github.com>
2018-04-23 18:18:29 +0100

    with setosa.csv

--- setosa.py
+++ setosa.py
@@ -4,8 +4,11 @@
 
 import numpy 
 
-data = numpy.genfromtxt('data/iris.csv', delimiter = ',')
+#I created another csv file after having trouble splitting up the columns to just the setosa file.
+data = numpy.genfromtxt('data/setosa.csv', delimiter = ',')
 
-setosa = data[0:50]
+setosa = data[:,0]
 
+#excludes nan from the file so we can use numpy to develop mean etc. https://stackoverflow.com/questions/11620914/removing-nan-values-from-an-array
+setosa = setosa[~numpy.isnan(setosa)]
 print(setosa)
